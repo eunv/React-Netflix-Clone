b84b6645e9262344db5a6fe0a1186d8238bf071e
eunv <dmsql1091@naver.com>
2023-10-25 16:38:18 +0900

    video func update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react'
 import requests from '../api/requests';
 import './Banner.css'
+import styled from 'styled-components';
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
@@ -28,8 +29,8 @@ export default function Banner() {
   };
 
   const truncate = (str, n) => {
-    return str?.length > n ? str.sbstr(0, n - 1) + "..." : str;
-  }
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
 
   if (!isClicked) {
     return (
@@ -38,7 +39,7 @@ export default function Banner() {
         style={{
           backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
           backgroundPosition: "top center",
-          backoroundSize: "cover"
+          backgroundSize: "cover"
         }}
       >
         <div className='banner__contents'>
@@ -56,7 +57,9 @@ export default function Banner() {
             <button className='banner__button info'>More Information</button>
           </div>
 
-          <h1 className='banner__description'>{movie.overview}</h1>
+          <h1 className='banner__description'>
+            {truncate(movie.overview, 100)}
+          </h1>
         </div>
         <div className='banner--fadeBottom' />
 
@@ -64,9 +67,48 @@ export default function Banner() {
     )
   } else {
     return (
-      <div>
-        clicked
-      </div>
+      <Container>
+        <HomeContainer>
+          <Iframe
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+          ></Iframe>
+        </HomeContainer>
+      </Container>
     )
   }
 }
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  // opacity: 0.65;
+  border: none;
+
+  &::after {
+    content:"";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height:100%;
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+
+`
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
-
+import "./Row.css";
 
 export default function Row({ title, id, fetchUrl, isLargeRow }) {
 
@@ -8,7 +8,7 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
 
   useEffect(() => {
     fetchMovieData();
-  },[]);
+  }, []);
 
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
@@ -16,6 +16,30 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
   }
 
   return (
-    <div></div>
+    <section>
+      <h2>{title}</h2>
+      <div className='silder'>
+        <div className='slider__arrow-left'>
+          <span className='arrow'>
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className='row__posters'>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className='slider__arrow-right'>
+          <span className='arrow'>
+            {">"}
+          </span>
+        </div>
+      </div>
+    </section>
   )
 }
