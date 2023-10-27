4fcf63fe36876d53846586eeadfd9b40a9cc706e
eunv <dmsql1091@naver.com>
2023-10-27 21:46:53 +0900

    search funtion update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './Nav.css';
 
 export default function Nav() {
   const [show, setShow] = useState(false);
-  // const [searchValue, setSearchValue] = useState("");
-  // const navigate = useNavigate();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -21,10 +21,10 @@ export default function Nav() {
     };
   }, []);
 
-  // const handleChange = (e) => {
-  //   setSearchValue(e.target.value);
-  //   navigate(`/search?q=${e.target.value}`);
-  // }; 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }; 
 
   return (
     <nav className= {`nav ${show && "nav__black"}`}>
@@ -34,13 +34,13 @@ export default function Nav() {
         className='nav__logo'
         onClick={() => (window.location.href = "/")}
       />
-      {/* <input
+      <input
         value={searchValue}
         onChange={handleChange}
         className="nav__input"
         type="text"
         placeholder="영화를 검색해주세요."
-      /> */}
+      />
       <img
         alt='User logged'
         src='https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-dyrp6bw6adbulg5b.webp'
---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,67 @@
+import axios from '../../api/axios';
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import "./SearchPage.css";
+
+export default function SearchPage() {
+
+  const [searchResults, setsearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if(searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async(searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&auery=${searchTerm}`
+      )
+      console.log(request);
+      setsearchResults(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  const renderSearchResults=() => {
+    return searchResults.length > 0 ? (
+      <section className='search-container'>
+        {searchResults.map((movie)=> {
+          if(movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+            "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+            return (
+              <div className='movie'>
+                <div className='movie__colum-poster'>
+                  <img 
+                    src = {movieImageUrl} alt='movie'
+                    className='movie__poster'
+                  />
+                </div>
+              </div>
+            )
+          }
+        })}
+      </section>
+    ) : (
+      <section className='no-results'>
+      <div className='no-results__text'>
+      <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+      </div>
+      
+      </section>
+    );
+  };
+  
+
+  return renderSearchResults();
+}
